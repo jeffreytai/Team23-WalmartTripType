f36c2eb9c3a16f43ccff8fc19b60520e5658e023
briancc23 <briancc23@yahoo.com>
2015-11-28 16:19:54 -0800

    Cluster triptypes together from training data

--- cluster2.py
+++ cluster2.py
@@ -64,8 +64,8 @@ def create_training_data_cluster(List, tripType):
         finelineDict = {}
         List.append(0)		#support
         List.append(0)		#visits
-        #for row in reader
-        for i in range(0,10000):
+        for row in reader:
+        #for i in range(0,10000):
         	prevRow = row
         	row = reader.next()
 
@@ -93,7 +93,7 @@ def create_training_data_cluster(List, tripType):
 	     
 
         AvgRows = float(List[0])/float(List[1])
-        print AvgRows
+        
         List.append(AvgRows)			#average rows per visit of that triptype
         List.append(upcDict)
         List.append(departmentDict)
@@ -102,44 +102,44 @@ def create_training_data_cluster(List, tripType):
     csvfile.close()
     return
 
-# create_training_data_cluster(TripType3, 3)
-# create_training_data_cluster(TripType4, 4)
-# create_training_data_cluster(TripType5, 5)
-# create_training_data_cluster(TripType6, 6)
-# create_training_data_cluster(TripType7, 7)
-# create_training_data_cluster(TripType8, 8)
-# create_training_data_cluster(TripType9, 9)
-# create_training_data_cluster(TripType12, 12)
-# create_training_data_cluster(TripType14, 14)
-# create_training_data_cluster(TripType15, 15)
-# create_training_data_cluster(TripType18, 18)
-# create_training_data_cluster(TripType19, 19)
-# create_training_data_cluster(TripType20, 20)
-# create_training_data_cluster(TripType21, 21)
-# create_training_data_cluster(TripType22, 22)
-# create_training_data_cluster(TripType23, 23)
-# create_training_data_cluster(TripType24, 24)
-# create_training_data_cluster(TripType25, 25)
+create_training_data_cluster(TripType3, 3)
+create_training_data_cluster(TripType4, 4)
+create_training_data_cluster(TripType5, 5)
+create_training_data_cluster(TripType6, 6)
+create_training_data_cluster(TripType7, 7)
+create_training_data_cluster(TripType8, 8)
+create_training_data_cluster(TripType9, 9)
+create_training_data_cluster(TripType12, 12)
+create_training_data_cluster(TripType14, 14)
+create_training_data_cluster(TripType15, 15)
+create_training_data_cluster(TripType18, 18)
+create_training_data_cluster(TripType19, 19)
+create_training_data_cluster(TripType20, 20)
+create_training_data_cluster(TripType21, 21)
+create_training_data_cluster(TripType22, 22)
+create_training_data_cluster(TripType23, 23)
+create_training_data_cluster(TripType24, 24)
+create_training_data_cluster(TripType25, 25)
 create_training_data_cluster(TripType26, 26)
-# create_training_data_cluster(TripType27, 27)
-# create_training_data_cluster(TripType28, 28)
-# create_training_data_cluster(TripType29, 29)
-# create_training_data_cluster(TripType30, 30)
-# create_training_data_cluster(TripType31, 31)
-# create_training_data_cluster(TripType32, 32)
-# create_training_data_cluster(TripType33, 33)
-# create_training_data_cluster(TripType34, 34)
-# create_training_data_cluster(TripType35, 35)
-# create_training_data_cluster(TripType36, 36)
-# create_training_data_cluster(TripType37, 37)
-# create_training_data_cluster(TripType38, 38)
-# create_training_data_cluster(TripType39, 39)
-# create_training_data_cluster(TripType40, 40)
-# create_training_data_cluster(TripType41, 41)
-# create_training_data_cluster(TripType42, 42)
-# create_training_data_cluster(TripType43, 43)
-# create_training_data_cluster(TripType44, 44)
-# create_training_data_cluster(TripType999, 999)
+create_training_data_cluster(TripType27, 27)
+create_training_data_cluster(TripType28, 28)
+create_training_data_cluster(TripType29, 29)
+create_training_data_cluster(TripType30, 30)
+create_training_data_cluster(TripType31, 31)
+create_training_data_cluster(TripType32, 32)
+create_training_data_cluster(TripType33, 33)
+create_training_data_cluster(TripType34, 34)
+create_training_data_cluster(TripType35, 35)
+create_training_data_cluster(TripType36, 36)
+create_training_data_cluster(TripType37, 37)
+create_training_data_cluster(TripType38, 38)
+create_training_data_cluster(TripType39, 39)
+create_training_data_cluster(TripType40, 40)
+create_training_data_cluster(TripType41, 41)
+create_training_data_cluster(TripType42, 42)
+create_training_data_cluster(TripType43, 43)
+create_training_data_cluster(TripType44, 44)
+create_training_data_cluster(TripType999, 999)
 
 print TripType26
 
